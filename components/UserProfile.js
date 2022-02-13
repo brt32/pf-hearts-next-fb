8e1c74f3b7377a5f108e08a0a6797ef337ac215c
Bartek <bartlomiej.proczkowski@gmail.com>
2022-02-13 02:07:13 +0100

    Small CSS changes

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,16 +1,24 @@
+import { useContext } from "react";
+import { UserContext } from "../lib/context";
+
 // UI component for user profile
 export default function UserProfile({ user }) {
+  const { username } = useContext(UserContext);
   return (
     <div className="box-center">
       <img src={user.photoURL || "/hacker.png"} className="card-img-center" />
       <p>
         <i>@{user.username}</i>
-        <p>
-          Do you want to change your username? Haha, it is imposible right now!
-          Maybe later...
-        </p>
+        {user.username === username && (
+          <p className="text-small">
+            Do you want to change your username? Haha, it is imposible right
+            now! Maybe later...
+          </p>
+        )}
       </p>
-      <h1>{user.displayName || "Anonymous User"}</h1>
+      <h2>
+        Ok, {user.displayName || "Anonymous User"}, here are your all posts:
+      </h2>
     </div>
   );
 }
